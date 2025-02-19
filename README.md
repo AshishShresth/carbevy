9f4bf1aec6981356bfe93adbaa159543684b4189
Ashish Shrestha <ashish11stha@gmail.com>
2025-02-19 21:49:43 +0545

    feat: webpack config

---
+++ README.md
@@ -0,0 +1,21 @@
+## 🛠 Installation & Set Up
+
+1. Install dependencies
+
+```sh
+npm install
+```
+
+2. Start the development server
+
+```sh
+npm run dev
+```
+
+## 🚀 Building for Production
+
+1. Generate the production build
+
+```sh
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.[contenthash].js',
+        clean: true,
+        assetModuleFilename: 'images/[name].[hash][ext][query]'
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Ashish Shrestha',
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            }
+        ],
+    },
+}
